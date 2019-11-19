b96e90bfbca9687e92ec139e8f2fd4492941c9f4
Stefan Schotvanger <s.schotvanger@gmail.com>
2019-11-19 22:44:11 +0100

    Ghost

--- src/GhostPost.php
+++ src/GhostPost.php
@@ -7,6 +7,8 @@ namespace RedirectDeletedProducts;
 class GhostPost
 {
 
+    private $filter;
+
     public function __construct()
     {
         $this->filter = new Filter();
@@ -19,8 +21,27 @@ class GhostPost
     public function saveGhost($id)
     {
         if (get_post_type($id) === 'product') {
-            $redirectFile = fopen(wp_upload_dir()['basedir'] . '/redirect-deleted-products.txt', "w");
-            fwrite($redirectFile,  get_the_permalink($id) . "\n");
+            $url = str_replace('__trashed', '', get_the_permalink($id));
+            $url = str_replace(get_site_url(), '', $url);
+
+            $product = wc_get_product($id);
+            $terms = $product->get_category_ids();
+
+            $category_urls = array();
+            if (!empty($terms)) {
+                foreach ($terms as $term) {
+                    $category_urls[] = str_replace(get_site_url(), '', get_term_link($term->term_id, 'product_cat'));
+                }
+            }
+
+            $info = array(
+                'url' => $url,
+                'category' => implode('@', $category_urls),
+                'shop' => str_replace(get_site_url(), '', get_permalink(wc_get_page_id('shop')))
+            );
+
+            $redirectFile = fopen(wp_upload_dir()['basedir'] . '/redirect-deleted-products.txt', "a+");
+            fwrite($redirectFile, implode('|', $info) . "\n");
             fclose($redirectFile);
         }
     }
